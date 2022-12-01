4a1b5eee11d7698a013197412de70b8743378272
CozmixOff <cozmix.n2@gmail.com>
2022-12-01 15:17:46 +0100

    Day 1 - Puzzle 2

---
+++ day 1/puzzle 2.py
@@ -0,0 +1,22 @@
+f = open('day 1/values.txt','r')
+file = f.read()
+values = file.split('\n')
+most,second,third,sum = (0,0,0,0)
+
+for i in range (len(values)):
+    if values[i] == '':
+        if sum > most:
+            third = second
+            second = most
+            most = sum
+        elif sum > second:
+            third = second
+            second = sum
+        elif sum > third:
+            third = sum
+        sum = 0
+    else:
+        sum += int(values[i])
+
+total = most + second + third
+print (total)
