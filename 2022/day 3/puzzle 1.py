e2feecdb9980f7952a352dc1eece41f071505e3b
CozmixOff <cozmix.n2@gmail.com>
2022-12-12 15:10:36 +0100

    Day 3 - Puzzle 1

---
+++ 2022/day 3/puzzle 1.py
@@ -0,0 +1,26 @@
+f = open('2022/day 3/values.txt','r')
+file = f.read()
+values = file.split('\n')
+dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+doubles = []
+score = 0
+
+for i in range (len(values)):
+    used = []
+    value = values[i]
+    start = []
+    for j in range (int(len(value)/2)):
+        start.append(value[j])
+    end = []
+    for j in range (int(len(value)/2),len(value)):
+        end.append(value[j])
+    for j in range (len(start)):
+        if end[j] in start:
+            doubles.append(end[j])
+            break
+
+for i in range (len(doubles)):
+    score += dict.index(doubles[i]) + 1
+    print (dict.index(doubles[i]),doubles[i])
+
+print (score)
